488674007011aa47965e8d64111ce7d6ffd9e691
Jan Jaap <jappyjan@users.noreply.github.com>
2025-08-17 21:15:54 +0200

    fix: ci release failing (#109)

--- apps/api/src/database/datasource.ts
+++ apps/api/src/database/datasource.ts
@@ -6,67 +6,20 @@ import { join, resolve } from 'path';
 import { entities } from '@attraccess/database-entities';
 import * as migrations from './migrations';
 
-const envType = loadEnv((z) => ({
-  DB_TYPE: z.enum(['postgres', 'sqlite']).default('sqlite'),
-}));
+const storageEnv = loadEnv((z) => ({ STORAGE_ROOT: z.string().default(join(process.cwd(), 'storage')) }));
+const dbFile = resolve(join(storageEnv.STORAGE_ROOT, 'attraccess.sqlite'));
 
-let dbConfig: Partial<DataSourceOptions> = {
-  type: envType.DB_TYPE,
+console.log('dbFile', dbFile);
+
+const dbConfig: Partial<DataSourceOptions> = {
   synchronize: false,
   migrations: Object.values(migrations),
   migrationsTableName: 'migrations',
   migrationsRun: true,
   entities: Object.values(entities),
-};
-
-function loadPostgresConfig() {
-  const env = loadEnv((z) => ({
-    DB_HOST: z.string(),
-    DB_PORT: z
-      .string()
-      .refine((port: string) => parseInt(port) > 0 && parseInt(port) < 65536)
-      .transform((port: string) => parseInt(port)),
-    DB_USERNAME: z.string(),
-    DB_PASSWORD: z.string(),
-    DB_DATABASE: z.string(),
-  }));
-
-  dbConfig = {
-    ...dbConfig,
-    type: 'postgres',
-    host: env.DB_HOST,
-    port: env.DB_PORT,
-    username: env.DB_USERNAME,
-    password: env.DB_PASSWORD,
-    database: env.DB_DATABASE,
-  } as DataSourceOptions;
-}
-
-function loadSqliteConfig() {
-  const storageEnv = loadEnv((z) => ({ STORAGE_ROOT: z.string().default(join(process.cwd(), 'storage')) }));
-  const dbFile = resolve(join(storageEnv.STORAGE_ROOT, 'attraccess.sqlite'));
-
-  console.log('dbFile', dbFile);
-
-  dbConfig = {
-    ...dbConfig,
-    type: 'sqlite',
-    database: dbFile,
-  } as DataSourceOptions;
-}
-
-switch (envType.DB_TYPE) {
-  case 'postgres':
-    loadPostgresConfig();
-    break;
-
-  case 'sqlite':
-    loadSqliteConfig();
-    break;
-
-  default:
-    throw new Error('Unknown Database type in DB_TYPE env');
-}
+  type: 'sqlite',
+  database: dbFile,
+} as DataSourceOptions;
 
 export const dataSourceConfig = dbConfig;
 
